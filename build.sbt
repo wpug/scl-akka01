2d01a6303c61d25c1838a5dfd8d4d0f26080367d
Wiesław Pawłowski <w.pawlowski@inf.ug.edu.pl>
2015-12-07 23:38:04 +0000

    Import szkieletu aplikacji do Git-a

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "AkkaIntro"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.4.1"
---
+++ src/main/scala/Prog.scala
@@ -0,0 +1,16 @@
+object Prog {
+	import akka.actor._
+
+	class MyActor extends Actor {
+		def receive = {
+			case msg => println("Otrzymałem komunikat: „" + msg.toString + "”")
+		}
+	}
+
+	def main(args: Array[String]): Unit = {
+		println("Hello Akka!")
+		val system = ActorSystem("system")
+		val john = system.actorOf(Props[MyActor], "john")
+		john ! 123
+	}
+}
